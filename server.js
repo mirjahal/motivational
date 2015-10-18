91820d1bbd6e41626b2058e4fb1579b09296a1df
Almir Jr <mirjahal@hotmail.com>
2015-10-18 18:59:32 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+var HapiRouter  = require('hapi-router');
+var routePath = 'app/routes/*.js';
+
+server.connection({
+    host: '0.0.0.0',
+    port: 3004,
+    routes: {
+        cors: true
+    }
+});
+
+server.register({
+    register: HapiRouter,
+    options: {
+        routes: routePath
+    }
+}, function (err) {
+    if (err) {
+        throw err;
+    }
+});
+
+server.start(function() {
+	console.log('Motivational is running');
+});
+
